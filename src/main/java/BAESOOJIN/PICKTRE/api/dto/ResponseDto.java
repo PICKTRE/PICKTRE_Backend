1ffb2bbcc8791ab87a2668c3c592144245eb5380
gytjd <hys339631@gmail.com>
2023-09-02 16:51:42 +0900

    [Feat] : ResponseDto 생성

---
+++ src/main/java/BAESOOJIN/PICKTRE/api/dto/ResponseDto.java
@@ -0,0 +1,15 @@
+package BAESOOJIN.PICKTRE.api.dto;
+
+
+import lombok.Data;
+
+@Data
+public class ResponseDto {
+
+    private String message;
+
+    public ResponseDto(String message) {
+        this.message = message;
+    }
+}
+
