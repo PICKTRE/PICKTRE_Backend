8dc74ff498b2fa528819f634e84450253da3afba
gytjd <hys339631@gmail.com>
2023-09-02 16:31:31 +0900

    [Feat] : RewardTransactionService 리워드 차감 및 증가 기능 추가

--- src/main/java/BAESOOJIN/PICKTRE/transaction/RewardTransactionRepository.java
+++ src/main/java/BAESOOJIN/PICKTRE/transaction/RewardTransactionRepository.java
@@ -1,10 +1,11 @@
 package BAESOOJIN.PICKTRE.transaction;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface RewardTransactionRepository {
+public interface RewardTransactionRepository extends JpaRepository<RewardTransaction,Long> {
     List<RewardTransaction> findByMemberId(Long memberId);
 }
