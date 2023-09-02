0d7298c49a59c4a8351b007e1dc6e5c9762182c7
gytjd <hys339631@gmail.com>
2023-09-02 15:16:52 +0900

    [Init] : 프로젝트 초기 설정

---
+++ src/test/java/BAESOOJIN/PICKTRE/PicktreApplicationTests.java
@@ -0,0 +1,13 @@
+package BAESOOJIN.PICKTRE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicktreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
