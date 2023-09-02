3010ba13151be1e6b71964c6cb19af222795b7f7
gytjd <hys339631@gmail.com>
2023-09-02 16:13:39 +0900

    [Feat] : OrderitemRepository 생성

---
+++ src/main/java/BAESOOJIN/PICKTRE/repository/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package BAESOOJIN.PICKTRE.repository;
+
+
+import BAESOOJIN.PICKTRE.domain.order.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
