5c56f9a3697f2b63220ff28f116ef3fb70c2d86f
gytjd <hys339631@gmail.com>
2023-09-02 15:55:41 +0900

    [Feat] : Member,Order,Product Domain 연관관계 설정

---
+++ src/main/java/BAESOOJIN/PICKTRE/domain/product/Product.java
@@ -0,0 +1,39 @@
+package BAESOOJIN.PICKTRE.domain.product;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name; // 상품 이름
+    private int price; // 상품 가격
+    private int quantity; // 상품 수량
+    private String imagePath; // 상품 이미지 파일 경로
+
+    private int viewCount;
+
+    @Builder
+    public Product(String name,String imagePath,int price, int quantity,int viewCount) {
+        this.name = name;
+        this.imagePath=imagePath;
+        this.price = price;
+        this.quantity = quantity;
+        this.viewCount=viewCount;
+    }
+}
+
