891e0549eda8fac15f692c2e88db94afbcd6849c
gytjd <hys339631@gmail.com>
2023-09-02 17:39:34 +0900

    [Feat] : ProductService 상품에 이미지 경로 추가 및 상품 등록 기능 추가

--- src/main/java/BAESOOJIN/PICKTRE/service/ProductService.java
+++ src/main/java/BAESOOJIN/PICKTRE/service/ProductService.java
@@ -1,5 +1,6 @@
 package BAESOOJIN.PICKTRE.service;
 
+import BAESOOJIN.PICKTRE.api.dto.product.ProductRequest;
 import BAESOOJIN.PICKTRE.domain.product.Product;
 import BAESOOJIN.PICKTRE.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,13 +9,33 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
+
+    /**
+     * 상품 등록
+     * <p>
+     * //     * @param name     상품 이름
+     * //     * @param price    상품 가격
+     * //     * @param quantity 상품 재고 수량
+     *
+     * @return 등록된 상품 엔티티
+     */
+
+    public Product createProduct(ProductRequest productRequest, String filePath) {
+        Product product = Product.builder()
+                .name(productRequest.getProductName())
+                .imagePath(filePath) // 이미지 파일 경로 추가
+                .price(productRequest.getPrice())
+                .quantity(productRequest.getQuantity())
+                .viewCount(0)
+                .build();
+        return productRepository.save(product);
+    }
 
     /**
      * 모든 상품 목록 조회
@@ -29,6 +50,23 @@ public class ProductService {
         return productRepository.findAll(Sort.by(Sort.Direction.DESC,"viewCount"));
     }
 
+    /**
+     * 상품 재고 수량 업데이트
+     *
+     * @param productId 상품 ID
+    //     * @param quantity  업데이트할 재고 수량
+     * @return 업데이트된 상품 엔티티
+     */
+    public Product updateProductQuantity(Long productId, ProductRequest productRequest) {
+
+        Product existingProduct = productRepository.findById(productId)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+
+        existingProduct.setName(productRequest.getProductName());
+        existingProduct.setPrice(productRequest.getPrice());
+        existingProduct.setQuantity(productRequest.getQuantity());
+        return productRepository.save(existingProduct);
+    }
 
     /**
      * 상품 삭제
@@ -70,4 +108,3 @@ public class ProductService {
         product.setViewCount(tempCount+1);
     }
 }
-
