53e78fdfb54ad72453d49802e534cc33711c4b76
gytjd <hys339631@gmail.com>
2023-09-03 14:59:11 +0900

    [Feat] : GoogleAuth AccessToken Getter,Setter 구현

---
+++ src/main/java/BAESOOJIN/PICKTRE/auth/GoogleAuth.java
@@ -0,0 +1,58 @@
+package BAESOOJIN.PICKTRE.auth;
+
+import BAESOOJIN.PICKTRE.api.dto.auth.GoogleOAuthTokenDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+@Getter
+public class GoogleAuth {
+
+    private final String googleLoginUrl = "https://accounts.google.com";
+    private final String GOOGLE_TOKEN_REQUEST_URL = "https://oauth2.googleapis.com/token";
+    private final String GOOGLE_USERINFO_REQUEST_URL = "https://www.googleapis.com/oauth2/v1/userinfo";
+    private final ObjectMapper objectMapper;
+    private final RestTemplate restTemplate;
+
+
+    @Value("${google.cliendId}")
+    private String googleClientId;
+    @Value("${google.redirect}")
+    private String googleRedirectUrl;
+    @Value("${google.secret}")
+    private String googleClientSecret;
+
+    public ResponseEntity<String> requestAccessToken(String code) {
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, Object> params = new HashMap<>();
+        params.put("code", code);
+        params.put("client_id", googleClientId);
+        params.put("client_secret", googleClientSecret);
+        params.put("redirect_uri", googleRedirectUrl);
+        params.put("grant_type", "authorization_code");
+
+        ResponseEntity<String> reponseEntity = restTemplate.postForEntity(GOOGLE_TOKEN_REQUEST_URL, params, String.class);
+
+        if (reponseEntity.getStatusCode() == HttpStatus.OK) {
+            return reponseEntity;
+        }
+        return null;
+    }
+
+    public GoogleOAuthTokenDto getAccessToken(ResponseEntity<String> response) throws JsonProcessingException { // accessToken에 대한 정보를 자바 객체로 저장후 리턴
+        System.out.println("respone.getBody()= " + response.getBody());
+        GoogleOAuthTokenDto googleOAuthTokenDto = objectMapper.readValue(response.getBody(), GoogleOAuthTokenDto.class);
+        return googleOAuthTokenDto;
+    }
+}
