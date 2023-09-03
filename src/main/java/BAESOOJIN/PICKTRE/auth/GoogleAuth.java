2502f9b84543e665ed69889d835ed0a777aa1f3d
gytjd <hys339631@gmail.com>
2023-09-03 15:03:01 +0900

    [Feat] : GoogleAuth UserInfo request 기능 구현

--- src/main/java/BAESOOJIN/PICKTRE/auth/GoogleAuth.java
+++ src/main/java/BAESOOJIN/PICKTRE/auth/GoogleAuth.java
@@ -7,9 +7,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
@@ -55,6 +55,15 @@ public class GoogleAuth {
         System.out.println("respone.getBody()= " + response.getBody());
         GoogleOAuthTokenDto googleOAuthTokenDto = objectMapper.readValue(response.getBody(), GoogleOAuthTokenDto.class);
         return googleOAuthTokenDto;
+    }
+
+    public ResponseEntity<String> requestUserInfo(GoogleOAuthTokenDto oAuthToken){// 토큰을 가지고 사용자 정보를 요청
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization","Bearer "+oAuthToken.getAccess_token());
+        HttpEntity<MultiValueMap<String,String>> request = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(GOOGLE_USERINFO_REQUEST_URL, HttpMethod.GET,request,String.class);
+        System.out.println("response.getBody() = "+response.getBody());
+        return response;
     }
 
     public GoogleUserInfoDto getUserInfo(ResponseEntity<String> response) throws JsonProcessingException{
