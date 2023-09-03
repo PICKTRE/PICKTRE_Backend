42fb4467f49890cc98aa80c45b1ea45c469d0abb
gytjd <hys339631@gmail.com>
2023-09-03 14:52:10 +0900

    [Feat] : GoogleOAuthTokenDto 생성

---
+++ src/main/java/BAESOOJIN/PICKTRE/api/dto/auth/GoogleOAuthTokenDto.java
@@ -0,0 +1,16 @@
+package BAESOOJIN.PICKTRE.api.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class GoogleOAuthTokenDto {
+    private String access_token;
+    private Integer expries_in;
+    private String scope;
+    private String token_type;
+    private String id_token;
+}
