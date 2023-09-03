e4e0fecba36ae021f2cd849dd5e75e490ac08ce2
gytjd <hys339631@gmail.com>
2023-09-03 15:03:53 +0900

    [Feat] : OAuthService GoogleUserInfo Getter 기능 구현

--- src/main/java/BAESOOJIN/PICKTRE/auth/OAuthService.java
+++ src/main/java/BAESOOJIN/PICKTRE/auth/OAuthService.java
@@ -1,10 +1,14 @@
 package BAESOOJIN.PICKTRE.auth;
 
+import BAESOOJIN.PICKTRE.api.dto.auth.GoogleOAuthTokenDto;
+import BAESOOJIN.PICKTRE.api.dto.auth.GoogleUserInfoDto;
 import BAESOOJIN.PICKTRE.repository.MemberRepository;
 import BAESOOJIN.PICKTRE.service.MemberService;
 import BAESOOJIN.PICKTRE.service.ResponseService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Transactional
@@ -16,4 +20,15 @@ public class OAuthService {
     private final MemberService memberService;
     private final MemberRepository memberRepository;
     private final ResponseService responseService;
+    private String access_Token_user;
+
+    private GoogleUserInfoDto getGoogleUserInfoDto(String code) throws JsonProcessingException {
+        ResponseEntity<String> accessTokenResponse = googleAuth.requestAccessToken(code);
+        GoogleOAuthTokenDto oAuthToken = googleAuth.getAccessToken(accessTokenResponse);
+        access_Token_user=oAuthToken.getAccess_token();
+        ResponseEntity<String> userInfoResponse = googleAuth.requestUserInfo(oAuthToken);
+        GoogleUserInfoDto googleUser = googleAuth.getUserInfo(userInfoResponse);
+        return googleUser;
+    }
+
 }
